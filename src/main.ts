938cd3605480d5176239c2b859292e8740e2cd81
jordanneves <jordanneves27@gmail.com>
2025-12-05 20:20:23 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // opcional, mas recomendado
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
